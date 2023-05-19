58c4ac5b781f9bfd055ef982bc9d3385946753d0
Joeri Breedveld <joeri.breedveld123@gmail.com>
2023-05-19 16:41:50 +0200

    feat(sidebar): make sidebar with some basic content

---
+++ src/components/common/Sidebar.tsx
@@ -0,0 +1,38 @@
+const Sidebar = () => (
+  <nav className="flex min-h-screen min-w-[16rem] flex-col justify-between p-8">
+    <section>
+      <h1 className="text-xl font-medium">Find Flow</h1>
+    </section>
+    <section>
+      <ul className="flex flex-col gap-6">
+        <li>
+          <button className="w-full rounded-2xl border-2 border-[#2B8585] px-8 py-4 text-left font-semibold text-[#2B8585]">
+            Creëer quiz
+          </button>
+        </li>
+        <li>
+          <button className="w-full rounded-2xl bg-[#F39D10] px-8 py-4 text-left font-semibold text-white">
+            Quiz
+          </button>
+        </li>
+        <li>
+          <button className="w-full rounded-2xl px-8 py-4 text-left font-semibold text-[#808080]">
+            Help
+          </button>
+        </li>
+        <li>
+          <button className="w-full rounded-2xl px-8 py-4 text-left font-semibold text-[#808080]">
+            Over
+          </button>
+        </li>
+      </ul>
+    </section>
+    <section>
+      <button className="w-full rounded-2xl bg-[#2B8585] px-8 py-4 text-left font-semibold text-white">
+        Logout
+      </button>
+    </section>
+  </nav>
+);
+
+export default Sidebar;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,14 @@
 // Imports
 import { type NextPage } from "next";
+import Sidebar from "~/components/common/Sidebar";
 
 // Functions
-const Home: NextPage = () => <div>Home</div>;
+const Home: NextPage = () => (
+  <div className="flex">
+    <Sidebar />
+    <div className="h-screen w-full bg-gray-100"></div>
+  </div>
+);
 
 // Exports
 export default Home;
