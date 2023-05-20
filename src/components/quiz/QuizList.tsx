cbfab3079cb462f55fb2a6e8a053d6c644acbfae
Joeri Breedveld <joeri.breedveld123@gmail.com>
2023-05-20 13:18:48 +0200

    fix(quiz): fix flex settings for card

--- src/components/quiz/QuizList.tsx
+++ src/components/quiz/QuizList.tsx
@@ -17,7 +17,7 @@ const Quiz = () => (
     <article className="flex h-full flex-col">
       <section className="h-full rounded-t-2xl bg-gray-200"></section>
       <section className="flex flex-col gap-4 rounded-b-2xl bg-white px-8 py-6">
-        <section className="flex h-full gap-6">
+        <section className="flex h-full w-full justify-between gap-6">
           <h4 className="font-medium">Het leven van Karel Appel lorem ipsum</h4>
           <a href="#" className="text-sm text-[#2B8585] underline">
             Resultaten
