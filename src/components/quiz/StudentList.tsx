66c4bfd2918a69c10c90a6dea9852979807b87c3
Joeri Breedveld <joeri.breedveld123@gmail.com>
2023-05-22 13:51:34 +0200

    feat(results): make student list and student score things

---
+++ src/components/quiz/StudentList.tsx
@@ -0,0 +1,46 @@
+import classNames from "classnames";
+
+type TStudentProps = {
+  variant?: "selected";
+};
+
+const StudentList = () => (
+  <section className="w-full rounded-2xl bg-white p-8">
+    <h2 className="mb-8 text-xl font-bold">Studentenlijst</h2>
+    <div className="mb-8 flex justify-between font-bold">
+      <p>Leerling</p>
+      <p>Score</p>
+    </div>
+    <ul className="flex flex-col gap-4">
+      <Student variant="selected" />
+      <Student />
+      <Student />
+      <Student />
+      <Student />
+      <Student />
+      <Student />
+      <Student />
+    </ul>
+  </section>
+);
+
+const Student = ({ variant }: TStudentProps) => (
+  <li
+    className={classNames(
+      "flex items-center justify-between rounded-2xl px-6 py-4",
+      {
+        "bg-[#eee]": variant === "selected",
+      }
+    )}
+  >
+    <div className="flex items-center gap-4">
+      <div className="h-[40px] w-[40px] rounded-full bg-gray-500"></div>
+      <p>Student naam</p>
+    </div>
+    <div className="flex items-center gap-4">
+      <p>14/20</p>
+    </div>
+  </li>
+);
+
+export default StudentList;
--- src/pages/results.tsx
+++ src/pages/results.tsx
@@ -1,7 +1,7 @@
 // Imports
 import { type NextPage } from "next";
 import Sidebar from "~/components/common/Sidebar";
-import QuestionList from "~/components/quiz/QuestionList";
+import StudentList from "~/components/quiz/StudentList";
 import QuizInfo from "~/components/quiz/QuizInfo";
 import Leaderboard from "~/components/quiz/Leaderboard";
 
@@ -10,7 +10,7 @@ const Results: NextPage = () => (
   <div className="flex">
     <Sidebar />
     <div className="flex h-screen w-full bg-gray-100 p-6">
-      <QuestionList />
+      <StudentList />
       <div className="flex flex-col gap-6 pl-6">
         <QuizInfo />
         <Leaderboard />
