49a5b33fe239218ce8d897a1b41ab099e721159b
Joeri Breedveld <joeri.breedveld123@gmail.com>
2023-05-23 14:13:16 +0200

    feat(faq): make faq section without functionality

--- src/components/quiz/Faq.tsx
+++ src/components/quiz/Faq.tsx
@@ -1,7 +1,21 @@
 const Faq = () => (
-  <section className="h-full rounded-2xl bg-white p-8">Faq</section>
+  <section className="h-full rounded-2xl bg-white p-8">
+    <h2 className="mb-8 text-xl font-bold">Veelgestelde vragen</h2>
+    <ul>
+      <FaqItem />
+      <FaqItem />
+      <FaqItem />
+      <FaqItem />
+      <FaqItem />
+      <FaqItem />
+    </ul>
+  </section>
 );
 
-const FaqItem = () => <li>FaqItem</li>;
+const FaqItem = () => (
+  <li className="border-b border-[#eee] py-6">
+    <p className="font-semibold">Faq vraag voorbeeld placeholder</p>
+  </li>
+);
 
 export default Faq;
