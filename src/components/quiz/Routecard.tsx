aef95f61b1a13389b56b725755ebc3ea89415033
Duck1s <32653@ma-web.nl>
2023-05-26 11:55:06 +0200

    feat(routecard): try to rebuild for hover effect

--- src/components/quiz/Routecard.tsx
+++ src/components/quiz/Routecard.tsx
@@ -4,14 +4,7 @@ const Routecard = () => (
   <section className="flex h-full w-full flex-col gap-4 rounded-2xl bg-white p-8">
     <h2 className="mb-8 text-xl font-bold">Routekaart</h2>
     <Link href="/location" className="h-full w-full">
-      <div
-        className="h-full w-full rounded-2xl"
-        style={{
-          background: "url(/img/map-placeholder.jpg)",
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}
-      ></div>
+      <div className="h-full w-full rounded-2xl bg-[url(/img/map-placeholder.jpg)] bg-cover bg-center"></div>
     </Link>
   </section>
 );
