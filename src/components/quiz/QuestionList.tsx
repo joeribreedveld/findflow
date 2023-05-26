2d37f0dc44453b55a94c2d7d27c28ee5fbf64be9
Duck1s <32653@ma-web.nl>
2023-05-26 12:00:02 +0200

    feat(questionlistitem): rebuild questionlistitem component

--- src/components/quiz/QuestionList.tsx
+++ src/components/quiz/QuestionList.tsx
@@ -1,50 +1,19 @@
-import classNames from "classnames";
-import Link from "next/link";
-
-type TQuestionProps = {
-  variant?: "selected";
-};
+import QuestionListItem from "./QuestionListItem";
 
 const QuestionList = () => (
   <section className="w-full rounded-2xl bg-white p-8">
     <h2 className="mb-8 text-xl font-bold">Vragenlijst</h2>
     <ul className="flex flex-col gap-4">
-      <Question variant="selected" />
-      <Question />
-      <Question />
-      <Question />
-      <Question />
-      <Question />
-      <Question />
-      <Question />
+      <QuestionListItem variant="selected" />
+      <QuestionListItem />
+      <QuestionListItem />
+      <QuestionListItem />
+      <QuestionListItem />
+      <QuestionListItem />
+      <QuestionListItem />
+      <QuestionListItem />
     </ul>
   </section>
-);
-
-const Question = ({ variant }: TQuestionProps) => (
-  <li
-    className={classNames(
-      "flex items-center justify-between rounded-2xl px-6 py-4",
-      {
-        "bg-[#eee]": variant === "selected",
-      }
-    )}
-  >
-    <div className="flex items-center gap-8">
-      <p>1.</p>
-      <p>Hoe oud is Karel Appel</p>
-    </div>
-    <div className="flex items-center gap-4">
-      <Link href="/question">
-        <button className="rounded-lg bg-[#B2CEEE] px-4 py-2 text-xs font-bold text-[#054B7D]">
-          Edit
-        </button>
-      </Link>
-      <button className="rounded-lg bg-[#EFC4C4] px-4 py-2 text-xs font-bold text-[#A71616]">
-        Delete
-      </button>
-    </div>
-  </li>
 );
 
 export default QuestionList;
---
+++ src/components/quiz/QuestionListItem.tsx
@@ -0,0 +1,34 @@
+import classNames from "classnames";
+import Link from "next/link";
+
+type TQuestionListItemProps = {
+  variant?: "selected";
+};
+
+const QuestionListItem = ({ variant }: TQuestionListItemProps) => (
+  <li
+    className={classNames(
+      "flex items-center justify-between rounded-2xl px-6 py-4",
+      {
+        "bg-[#eee]": variant === "selected",
+      }
+    )}
+  >
+    <div className="flex items-center gap-8">
+      <p>1.</p>
+      <p>Hoe oud is Karel Appel</p>
+    </div>
+    <div className="flex items-center gap-4">
+      <Link href="/question">
+        <button className="rounded-lg bg-[#B2CEEE] px-4 py-2 text-xs font-bold text-[#054B7D]">
+          Edit
+        </button>
+      </Link>
+      <button className="rounded-lg bg-[#EFC4C4] px-4 py-2 text-xs font-bold text-[#A71616]">
+        Delete
+      </button>
+    </div>
+  </li>
+);
+
+export default QuestionListItem;
