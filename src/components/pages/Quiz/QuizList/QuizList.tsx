b3d02a4af60248f5a2582b442065dfb23259816e
Joeri Breedveld <joeri.breedveld123@gmail.com>
2023-05-30 16:37:42 +0200

    feat(api): make api endpoint for quizdata

--- src/components/pages/Quiz/QuizList/QuizList.tsx
+++ src/components/pages/Quiz/QuizList/QuizList.tsx
@@ -1,20 +1,43 @@
 // Imports
 import QuizListItem from "../QuizListItem/QuizListItem";
+import { useEffect, useState } from "react";
 
 // Functions
-const QuizList = () => (
-  <ul className="grid h-full grid-cols-3 gap-6 p-6">
-    <QuizListItem />
-    <QuizListItem />
-    <QuizListItem />
-    <QuizListItem />
-    <QuizListItem />
-    <QuizListItem />
-    <QuizListItem />
-    <QuizListItem />
-    <QuizListItem />
-  </ul>
-);
+async function getQuizData() {
+  const res = await fetch("/api/quiz");
+  const data = await res.json();
+
+  return data;
+}
+
+const QuizList = () => {
+  const [quizData, setQuizData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getQuizData();
+      setQuizData(data);
+
+      console.log(data);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <ul className="grid h-full grid-cols-3 gap-6 p-6">
+      <QuizListItem />
+      <QuizListItem />
+      <QuizListItem />
+      <QuizListItem />
+      <QuizListItem />
+      <QuizListItem />
+      <QuizListItem />
+      <QuizListItem />
+      <QuizListItem />
+    </ul>
+  );
+};
 
 // Exports
 export default QuizList;
