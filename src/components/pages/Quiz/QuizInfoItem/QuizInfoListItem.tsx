5a41bda42b7e066200ca09b36824d51390a06fe8
Joeri Breedveld <joeri.breedveld123@gmail.com>
2023-06-04 12:35:48 +0200

    feat(quizinfo): add date to quiz info

--- src/components/pages/Quiz/QuizInfoItem/QuizInfoListItem.tsx
+++ src/components/pages/Quiz/QuizInfoItem/QuizInfoListItem.tsx
@@ -1,7 +1,7 @@
 // Functions
 const QuizInfoListItem = ({ title, answer }: TQuizInfoListItemProps) => (
   <li>
-    <h3 className="mb-1 text-xl font-semibold">{answer}</h3>
+    <h3 className="mb-1 text-lg font-semibold">{answer}</h3>
     <p className="font-semibold text-[#2B8585]">{title}</p>
   </li>
 );
--- src/components/pages/Quiz/QuizInfoList/QuizInfoList.tsx
+++ src/components/pages/Quiz/QuizInfoList/QuizInfoList.tsx
@@ -1,12 +1,13 @@
 // Imports
 import Link from "next/link";
-import QuizInfoListItem from "../QuizInfoItem/QuizInfoItem";
+import QuizInfoListItem from "../QuizInfoItem/QuizInfoListItem";
 
 // Functions
 const QuizInfoList = () => (
   <section className="w-[30rem] rounded-2xl bg-white p-8">
-    <h2 className="mb-8 text-xl font-semibold">Quiz informatie</h2>
-    <ul className="mb-10 flex flex-col gap-6">
+    <h2 className="mb-2 text-xl font-semibold">Quiz informatie</h2>
+    <p className="mb-6">01-01-2023</p>
+    <ul className="mb-10 flex flex-col gap-4">
       <QuizInfoListItem title="Aantal vragen" answer="20 vragen" />
       <QuizInfoListItem title="Gemiddelde score" answer="16/20" />
       <QuizInfoListItem title="Gemaakt door" answer="Gert Jan" />
